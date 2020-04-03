02bf8264dc222449573023c99a3b774f1f4099ff
Ioan Hedeș <ihedes13@gmail.com>
2020-04-03 16:22:38 +0300

    Program care afla valoarea maxima dintr=un vector. Poate fi compilat in mod debug/release. Pe baza modului de compilare se afiseaza mai multe sau mai putine informatii la stderr.

---
+++ lab7/app2.c
@@ -0,0 +1,70 @@
+/*
+  Aplicația 7.3: Pentru exemplul 2, să se scrie în funcția vmax la început o
+                 buclă care afișează conținutul vectorului v. Toată această
+                 buclă va trebui să existe în program doar dacă este definit
+                 DEBUG sau _DEBUG.
+*/
+#include <stdio.h>
+#include <assert.h>
+#include "myStdlib.h"
+#include "arrayHandler.h"
+
+#if defined(DEBUG) || defined(_DEBUG)
+  #define DEB(...) fprintf(stderr, __VA_ARGS__)
+#else
+  #define NDEBUG 1 // < Switch for assert()
+  #define DEB(...)
+#endif
+
+create_array_handler(double)
+
+double maximumValue(const double * array, const unsigned * array_size)
+{
+  int i;
+  assert(array != NULL);
+  assert(*array_size > 0);
+
+  DEB_PRT_ARRAY(double, array, array_size);
+
+  double max = array [0];
+
+  for (i = 1; i < *array_size; i++)
+  {
+    if(max < array [i])
+    {
+      max = array [i];
+    }
+  }
+  return max;
+}
+
+int main()
+{
+  double * array = NULL;
+  unsigned array_size;
+  printf("Enter the number of elements the array will have: ");
+  if (!get_unsigned(&array_size, stdin))
+  {
+    fprintf(stderr, "Error when reading the number of array elements.\nExiting...\n");
+    exit(EXIT_FAILURE);
+  }
+  if ((array = createArray_double(&array_size)) == NULL)
+  {
+    fprintf(stderr, "Error when allocating memory for the array.\nExiting...\n");
+    exit(EXIT_FAILURE);
+  }
+  if (!fillArray_double(array, &array_size))
+  {
+    free(array);
+    MALLOC_COUNTER--;
+    fprintf(stderr, "Error when filling the array.\nExiting...\n");
+    exit(EXIT_FAILURE);
+  }
+
+  printf("The maximum value found in this array is: %lf\n", maximumValue(array, &array_size));
+
+  free(array);
+  MALLOC_COUNTER--;
+  DEB("Execution ended.\nUnreleased memory blocks: %d;\nExiting...\n", MALLOC_COUNTER);
+  return 0;
+}
