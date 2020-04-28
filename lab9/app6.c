1df365dd4f10f3a5ee573d177cb7b248be32f4d5
Ioan Hedeș <ihedes13@gmail.com>
2020-04-28 18:52:25 +0300

    Comentarii adaugate

--- lab9/app6.c
+++ lab9/app6.c
@@ -1,9 +1,48 @@
+/**
+  Aplicația 9.6: Să se scrie o funcție care primește ca parametri o listă
+                 (posibil vidă) de elemente sortate și un element.
+                 Funcția va insera în listă noul element, astfel încât lista să
+                 rămână sortată. Folosind aceasta funcție, să se scrie o funcție
+                 de sortare a unei liste, care primește ca parametru o listă
+                 nesortată și returnează una sortată. Programul nu va folosi
+                 niciun vector.
+*/
+
 #include <stdio.h>
 #include "myStdlib.h"
 #include "listHandler.h"
 
+// Call the list handler creation macro for the int type
+// Check listHandler.h for details
 create_list_handler(int)
 
+// Function to insert a value inside a sorted list
+/**
+  USAGE:
+    - insert a value inside a sorted list, while keeping it sorted;
+    - the function iterates the list up until the point where the element must
+      be inserted;
+    - then, it creates a new node and connects it there;
+    - in case the given input list is empty, a singleton with the element to be
+      added is created;
+
+  ARGUMENTS:
+    - Node_int * sorted_list - the sorted input list;
+    - const int * element - the value of the element to be inserted
+
+  RETURN VALUE:
+    - pointer to the list - success;
+    - NULL - failure when creating the new node;
+
+  SIDE EFFECTS:
+    - no outside values will be modified by this function;
+
+  WARNING:
+    - please only use this function in programs that add nodes to a list using
+      the add first method, or else the tail of the list will not be properly set
+    - if you don't know what this is, you should check the first lines of
+      listHandler.h
+*/
 Node_int * insertInSortedList(Node_int * sorted_list, const int * element)
 {
   Node_int * current = sorted_list;
@@ -18,7 +57,10 @@ Node_int * insertInSortedList(Node_int * sorted_list, const int * element)
     return OAF(int, sorted_list, element);
   }
   DEB("\t - finding the position for the element to be inserted;\n");
-  while(current && current->info > *element) // mandatory for current to be first inside the condition // segfault otherwise
+
+  // In the while condition underneath, it is mandatory to have the iterator check
+  // first. Else, you might check for the info field of a null pointer => segfault;
+  while(current && current->info > *element)
   {
     DEB("\t\t - passed element %d;\n", current->info);
     previous = current;
@@ -41,12 +83,32 @@ Node_int * insertInSortedList(Node_int * sorted_list, const int * element)
     previous->next = aux;
   }
   DEB("Done.\n");
+
   return sorted_list;
 }
 
-Node_int * sortList(Node_int * list)
+// Function to sort a list using the previous function
+/**
+  USAGE:
+    - returns a new list, created with the elements of the previous one,
+      but sorted;
+    - uses the insertInSortedList() function:
+        - it iterates the initial list, and calls this function for each element,
+          the result after each value being kept in a buffer list, which will
+          also serve as the return value of the sorting function
+    - if you want it to release the unsorted list, just add a freeList_int()
+      call before the end of the function, passing the initial list as a parameter;
+
+  ARGUMENTS:
+    - Node_int * list - the list to be sorted;
+
+  RETURN VALUE:
+    - success - a pointer to the new sorted list;
+    - failure - null pointer;
+*/
+Node_int * sortList(const Node_int * list)
 {
-  Node_int * iter = NULL;
+  const Node_int * iter = NULL;
   Node_int * sorted_list = NULL;
   Node_int * aux = NULL;
 
@@ -64,29 +126,38 @@ Node_int * sortList(Node_int * list)
   return sorted_list;
 }
 
+// Driver program for the sorting function
 int main()
 {
+  // Necessary variables
   unsigned list_size = 0;
   Node_int * list = NULL;
   Node_int * sorted_list = NULL;
 
+  // Creating & filling the list
   printf("Enter the desired size of the list: ");
   if (!get_unsigned(&list_size, stdin))
   {
+    // Treating input errors
     fprintf(stderr, "Input error when reading the list's size.\nExiting...\n");
     exit(EXIT_FAILURE);
   }
   if ((list = createList_int(&list_size)) == NULL)
   {
+    // Treating input or allocation errors
     fprintf(stderr, "The creation of the list failed.\nExiting...\n");
     exit(EXIT_FAILURE);
   }
   putchar('\n');
-  printList_int(list);
+  printList_int(list); // < Printing the list to check if all went good
 
   putchar('\n');
+
+  // Running the tested function
+  // Sorting the list...
   if ((sorted_list = sortList(list)) == NULL)
   {
+    // Treating function errors
     fprintf(stderr, "Failed to sort the initial list.\n");
     freeList_int(list);
     DEB("Unreleased memory blocks: %d;\n", MALLOC_COUNTER);
@@ -100,6 +171,7 @@ int main()
 
   putchar('\n');
 
+  // Free the memory and end execution
   freeList_int(list);
   freeList_int(sorted_list);
   printf("Execution ended.\n");
