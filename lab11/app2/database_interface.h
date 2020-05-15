216c685223563f97e669ecb2720cd203a60b5498
Ioan Hedeș <ihedes13@gmail.com>
2020-05-15 23:59:25 +0300

    Adaugat problema 2 din lab, care e de fapt problema 1 cu o caracteristica/utilitate in plus.

---
+++ lab11/app2/database_interface.h
@@ -0,0 +1,32 @@
+// Interface for declaring database functions
+
+#pragma once
+
+#define MAX_LENGTH 31
+
+typedef struct Person Person;
+
+struct Person
+{
+  char * name;
+  double wage;
+  Person * next;
+};
+
+extern Person * database;
+
+int addPerson(char * name, double wage);
+
+int deletePerson(char * name);
+
+int existsPerson(const char * name);
+
+void printDatabase();
+
+void printOrderedByWage();
+
+void freeDatabase();
+
+int saveDatabase();
+
+int loadDatabase();
---
+++ lab11/app2/fileHandler.c
@@ -0,0 +1,108 @@
+#include "database_interface.h"
+#include "utils.h"
+
+int saveDatabase()
+{
+  Person * current = NULL;
+  FILE * file = fopen("database.txt", "wb");
+
+  DEB("Saving database to file...\n");
+  if (!file)
+  {
+    DEB("Failed to create/open file;\n");
+    freeDatabase();
+    return 0;
+  }
+  OPENED_FILES_COUNTER++;
+  DEB("Iterating database to store it on disk...\n");
+  for (current = database; current; current = current->next)
+  {
+    DEB("\t - person name: %s;\n", current->name);
+    fprintf(file, "%s;%f\n", current->name, current->wage);
+  }
+  fclose(file);
+  OPENED_FILES_COUNTER--;
+  DEB("Files still open: %d;\n", OPENED_FILES_COUNTER);
+  DEB("Saving database complete;\n\n");
+  return 1;
+}
+
+int loadDatabase()
+{
+  FILE * file = fopen("database.txt", "rb");
+  int semicol_pos;
+  char * buffer_string = NULL;
+  double buffer_wage;
+
+  DEB("Loading database from file...\n");
+  if (!file)
+  {
+    DEB("File failed to open. This may be because the file does not exist,");
+    DEB("or because another error happened.\n");
+    return 0;
+  }
+  OPENED_FILES_COUNTER++;
+  freeDatabase();
+
+  while(!ferror(file))
+  {
+    if ((buffer_string = getString(file)) == NULL)
+    {
+      if (!feof(file))
+      {
+        DEB("\t - failed to read a name from the file;\n");
+        freeDatabase();
+        fclose(file);
+        OPENED_FILES_COUNTER--;
+        return -1;
+      }
+      else
+      {
+        DEB("\t - EOF of database file reached;\n");
+        break;
+      }
+    }
+    DEB("\t - new line from file: %s;\n", buffer_string);
+
+
+    semicol_pos = strcspn(buffer_string, ";");
+    if (semicol_pos == strlen(buffer_string))
+    {
+      DEB("\t\t - the line is invalid (no semicolon), ignoring it...\n");
+      free(buffer_string);
+      MALLOC_COUNTER--;
+      continue;
+    }
+    buffer_wage = atof(buffer_string + semicol_pos + 1);
+    buffer_string = (char *)realloc(buffer_string, semicol_pos + 1);
+    buffer_string [semicol_pos] = '\0';
+
+    DEB("\t\t - extracted name: %s;\n", buffer_string);
+    DEB("\t\t - extracted wage: %lf;\n", buffer_wage);
+
+    if (!addPerson(buffer_string, buffer_wage))
+    {
+      DEB("\t\t - failed to add person to database from file;\n");
+      free(buffer_string);
+      MALLOC_COUNTER--;
+      freeDatabase();
+      fclose(file);
+      OPENED_FILES_COUNTER--;
+      return -1;
+    }
+  }
+  if (ferror(file))
+  {
+    DEB("Input file error;\n");
+    freeDatabase(database);
+    fclose(file);
+    OPENED_FILES_COUNTER--;
+    return -1;
+  }
+
+  fclose(file);
+  OPENED_FILES_COUNTER--;
+  DEB("Files still open: %d;\n", OPENED_FILES_COUNTER);
+  DEB("Importing database complete;\n\n");
+  return 1;
+}
